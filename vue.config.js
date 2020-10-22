ee08506eb9645159b2ebeb1bfc1a150dc4d4a32f
braswelljr <braswellkenneth7@gmail.com>
2020-10-22 03:25:11 +0000

    files update

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    manifestCrossorigin: "use-credentials",
+    workboxPluginMode: "InjectManifest",
+    msTileColor: "#DFA689",
+    manifestOptions: {
+      background_color: "#FAD7D7"
+    }
+  },
+
+  productionSourceMap: false
+};
